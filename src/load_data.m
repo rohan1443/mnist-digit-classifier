49d070be13d6b561cbffabdb6e55fc46fea98b2a
Rohan Mazumdar <rohanm@Rohans-MacBook-Pro.local>
2026-03-19 11:25:20 +0530

    minor change of updating the loading data path when terminal running from root

--- src/load_data.m
+++ src/load_data.m
@@ -16,13 +16,13 @@ fprintf('Loading MNIST dataset...\n');
 
 % Read training data from CSV
 % Always run the code from the root directory of the project mnist-digit-classifier to avoid path issues
-% rootDir = pwd;
-% train_data = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_train.csv'));
-% test_data  = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_test.csv'));
-
-rootDir = fileparts(pwd);  % go to project root
+rootDir = pwd;
 train_data = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_train.csv'));
 test_data  = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_test.csv'));
+
+% rootDir = fileparts(pwd);  % go to project root
+% train_data = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_train.csv'));
+% test_data  = readmatrix(fullfile(rootDir, 'data', 'csv', 'mnist_test.csv'));
 
 fprintf('Files loaded successfully!\n\n');
 
